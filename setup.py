be2810cadee82bef3f7b1d12bfd28848ac130614
vuh <vha14@msn.com>
2017-03-06 14:23:39 -0800

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='alexafsm',
-    version='0.1.3',
+    version='0.1.4',
     description="Finite-state machine library for building complex Alexa conversations",
     long_description=readme + '\n\n' + history,
     author="Allen AI",
